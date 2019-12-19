4a84cc0a59c02bb1a1c30af5d53315acf8640389
Claire Bergey <clairebergey@gmail.com>
2019-12-18 23:59:22 -0600

    word2vec training and test files for COCA

---
+++ model/test_word2vec_COCA.py
@@ -0,0 +1,108 @@
+''' 
+Claire Bergey 2019
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import io
+import os
+from gensim import utils
+import gensim.models
+import gensim.models.word2vec
+from gensim.test.utils import datapath
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+import tempfile
+
+# download google dataset to get their test items to gauge accuracy later on 
+import gensim.downloader as api
+#google = api.load('word2vec-google-news-300')
+
+model = gensim.models.Word2Vec.load("coca_word2vec.model")
+
+pairs = [
+    ('car', 'minivan'),   
+    ('car', 'bicycle'),  
+    ('car', 'airplane'),  
+    ('car', 'pear'),   
+    ('car', 'justice'),
+    ('king', 'queen'),
+]
+
+for w1, w2 in pairs:
+  print('%r\t%r\t%.2f' % (w1, w2, model.wv.similarity(w1, w2)))
+
+#model.accuracy('./datasets/questions-words.txt')
+model.wv.evaluate_word_pairs(datapath('wordsim353.tsv'))
+
+from sklearn.decomposition import IncrementalPCA    # inital reduction
+from sklearn.manifold import TSNE                   # final reduction
+import numpy as np                                  # array handling
+
+
+def reduce_dimensions(model):
+    num_dimensions = 2  # final num dimensions (2D, 3D, etc)
+
+    vectors = [] # positions in vector space
+    labels = [] # keep track of words to label our data again later
+    for word in model.wv.vocab:
+        vectors.append(model.wv[word])
+        labels.append(word)
+
+    # convert both lists into numpy vectors for reduction
+    vectors = np.asarray(vectors)
+    labels = np.asarray(labels)
+
+    # reduce using t-SNE
+    vectors = np.asarray(vectors)
+    tsne = TSNE(n_components=num_dimensions, random_state=0)
+    vectors = tsne.fit_transform(vectors)
+
+    x_vals = [v[0] for v in vectors]
+    y_vals = [v[1] for v in vectors]
+    return x_vals, y_vals, labels
+
+
+x_vals, y_vals, labels = reduce_dimensions(model)
+
+def plot_with_plotly(x_vals, y_vals, labels, plot_in_notebook=True):
+    from plotly.offline import init_notebook_mode, iplot, plot
+    import plotly.graph_objs as go
+
+    trace = go.Scatter(x=x_vals, y=y_vals, mode='text', text=labels)
+    data = [trace]
+
+    if plot_in_notebook:
+        init_notebook_mode(connected=True)
+        iplot(data, filename='word-embedding-plot')
+    else:
+        plot(data, filename='word-embedding-plot.html')
+
+
+def plot_with_matplotlib(x_vals, y_vals, labels):
+    import matplotlib.pyplot as plt
+    import random
+
+    random.seed(0)
+
+    plt.figure(figsize=(12, 12))
+    plt.scatter(x_vals, y_vals)
+
+    #
+    # Label randomly subsampled 25 data points
+    #
+    indices = list(range(len(labels)))
+    selected_indices = random.sample(indices, 25)
+    for i in selected_indices:
+        plt.annotate(labels[i], (x_vals[i], y_vals[i]))
+    plt.show()
+    plt.savefig('small_COCA.png')
+
+try:
+    get_ipython()
+except Exception:
+    plot_function = plot_with_matplotlib
+else:
+    plot_function = plot_with_plotly
+
+plot_function(x_vals, y_vals, labels)
+
---
+++ model/train_word2vec_COCA.py
@@ -0,0 +1,39 @@
+''' 
+Claire Bergey 2019
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import io
+import os
+from gensim import utils
+import gensim.models
+import gensim.models.word2vec
+from gensim.test.utils import datapath
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+FILE_NAMES = []
+
+for year in range(1990,2013):
+   FILE_NAMES.append('w_spok_' + str(year) + '.txt')
+
+read_dir = '/Users/clairebergey/Box/COCA/texts/text_spoken_kde'
+
+exclude = set('@#1234567890()*')
+
+
+class corpus(object):
+  def __iter__(self):
+    for i, file_name in enumerate(FILE_NAMES):
+      with open(os.path.join(read_dir, file_name), 'r') as f:
+        for line in f:
+          line = line.lower().replace('.', ' . ').replace('/', ' ').replace(',',' ').replace('-',' ')
+          for word in line:
+            if any ((c in exclude) for c in word):
+              line.replace(word, '')
+          yield utils.simple_preprocess(line)
+
+sentences = corpus()
+model = gensim.models.Word2Vec(sentences=sentences)
+
+model.save("coca_word2vec.model")
